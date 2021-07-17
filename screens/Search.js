b63aaefd9a5210107907271b2e52c111ef1f62b6
prafulpathare <patharepraful@gmail.com>
2021-07-17 16:10:50 +0530

    added stack navigation in search screen with product screen

--- screens/Search.js
+++ screens/Search.js
@@ -1,13 +1,64 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { View, Text, Button, Image, StyleSheet, ImageBackground, TouchableOpacity } from 'react-native';
+
+const Stack = createStackNavigator();
+
+const ProductScreen = ({ navigation }) => {
+
+    return (
+        <View style={styles.mainContainer}>
+            <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+
+            <TouchableOpacity onPress={() => navigation.navigate('Search')} >
+                <Image source={require('../assets/icons/backArrow.png')} style={{
+                    width: 24, height: 24, margin: 15
+                }} />
+            </TouchableOpacity>
+                <Text style={{ height: 24, marginVertical: 15 }}>Product name</Text>
+            </View>
+                <ImageBackground resizeMode='contain'
+                    source={{
+                        uri: 'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-12-purple-select-2021?wid=470&hei=556&fmt=jpeg&qlt=95&.v=1617130317000'
+                    }}
+                    style={{
+                        height: 400,
+                        backgroundColor: '#fff'
+                    }}
+                />
+
+            <View style={{
+                padding: 10,
+                backgroundColor: '#fff',
+                marginTop: 3
+            }}>
+                <Text>Product name</Text>
+            </View>
+
+
+        </View>
+    )
+}
+const MainSearchScreen = ({ navigation }) => {
+
+    return (
+        <View style={styles.mainContainer}>
+            <Text>List of Products</Text>
+            <Button title='View Product' onPress={() => navigation.navigate('Product')} />
+        </View>
+    )
+}
 
 
 const SearchScreen = ({ navigation }) => {
     return (
-        <View style={ styles.mainContainer }>
-            <Text>Cart</Text>
-            <Button title="Place Order" onPress={() => alert('Order Placed')} />
-        </View>
+        <Stack.Navigator initialRouteName="Search">
+            <Stack.Screen name="Search" component={MainSearchScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="Product" component={ProductScreen} options={{
+                headerShown: false
+
+            }} />
+        </Stack.Navigator>
     )
 }
 
